2c98c747e4be96c04c03718881d33037de7117d4
Monica Ran <ranmonica96@gmail.com>
2023-10-19 10:45:12 -0700

    added html and js files with pseudocode

---
+++ index.js
@@ -0,0 +1,13 @@
+// Create a prompt for user to enter froyo flavors 
+
+// Parse user input into an array of froyo flavors 
+
+// Build an object to track which flavors you've observed
+
+// Create a loop to iterate through the array of flavors 
+
+// Build an object to keep count of how many orders there are of each flavor
+
+// Ensure the program correctly counts the # of each flavor in user's input
+
+// Console output changes depending on the user's input 
